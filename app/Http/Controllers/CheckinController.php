a24c873eb46e2b831abb33ddba0ffed7b727b5fe
YairAyalaM <platagugu@gmail.com>
2023-05-12 12:24:10 -0600

    add check in user

---
+++ app/Http/Controllers/CheckinController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class CheckinController extends Controller
+{
+    //Usuarios Autenticados
+    // public function store(Request $request)
+    // {
+    //     $user = Auth::user();
+    //     $input = $request->validate([
+    //         'user_id' => 'required|exists:users,id'
+    //     ]);
+    //     if ($user->id == $input['user_id']) {
+    //         return redirect()->back()->with('error', 'No puedes hacer check-in por ti mismo.');
+    //     }
+    //     $user_to_checkin = User::findOrFail($input['user_id']);
+    //     if ($user_to_checkin->status) {
+    //         return redirect()->back()->with('error', 'El usuario ya hizo check-in previamente.');
+    //     }
+    //     $user_to_checkin->status = true;
+    //     $user_to_checkin->save();
+    //     return redirect()->back()->with('success', 'Check-in registrado exitosamente.');
+    // }
+    
+    //usuarios sin autenticar
+    public function store(Request $request)
+    {
+        $input = $request->validate([
+            'user_id' => 'required|exists:users,id'
+        ]);
+
+        $user_to_checkin = User::findOrFail($input['user_id']);
+
+        // Verificar si el usuario ya ha realizado el check-in
+        if ($user_to_checkin->status) {
+            return redirect()->back()->with('error', 'Este usuario ya ha realizado el check-in anteriormente.');
+        }
+
+        // Actualizar el estado del usuario a "registrado"
+        $user_to_checkin->status = true;
+        $user_to_checkin->save();
+        
+
+        // Mostrar un mensaje de éxito
+        return redirect()->back()->with('success', 'Check-in registrado exitosamente.');
+    }
+}
---
+++ resources/views/check.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">{{ __('Registrar Check-in') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('checkin.store') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="user_id">{{ __('ID del usuario:') }}</label>
+                                <input id="user_id" type="text" class="form-control @error('user_id') is-invalid @enderror" name="user_id" value="{{ old('user_id') }}" required autofocus placeholder="{{ __('Ingresa el ID del usuario a hacer check-in') }}" style="margin-bottom: 10px;">
+
+                                @error('user_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-12 text-right">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Registrar Check-in') }}
+                                    </button>
+                                </div>
+                            </div>
+
+                        </form>
+
+                        @if(session('success'))
+                        <div class="alert alert-success mt-3">
+                            {{ session('success') }}
+                        </div>
+                        @endif
+
+                        @if(session('error'))
+                        <div class="alert alert-danger mt-3">
+                            {{ session('error') }}
+                        </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
